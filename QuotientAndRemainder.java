4b1eea20db368fef831b693ca4131824835a03ca
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-06 11:21:34 +0530

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,11 @@
+
+public class QuotientAndRemainder {
+	public static void main(String args[]) {
+		int a = 7;
+		int b = 3;
+		int quotient = a / b;
+		int remainder = a % b;
+		System.out.println("Quotient is: " + quotient);
+		System.out.println("Remainder is: " + remainder);
+	}
+}
