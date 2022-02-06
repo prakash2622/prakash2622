0e62fa91525132ffb4a1f95918c8f5b452824a0b
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-06 11:23:11 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class SwapTwoNumbers {
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 12;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+	}
+}
