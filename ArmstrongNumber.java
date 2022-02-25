2ab800bb29f9b4139f036703a1be19c7a1c2c756
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-25 15:33:53 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,6 +1,6 @@
 Armstrong Number // 371
 
-public class Hiii {
+public class ArmstrongNumber{
 	public static void main(String[] args) {
 		int number = 371, temp, remainder, result = 0;
 		temp = number;
@@ -14,5 +14,25 @@ public class Hiii {
 			System.out.println(number + " is Armstrong Number");
 		else
 			System.out.println(number + "is not Armstrong Number");
+	}
+}
+
+// Armstrong Number between 100 to 999 // 153, 370, 371, 407
+public class ArmstrongNumber{
+public static void main(String[] args) {
+		int i = 100, remain, arm, num;
+		System.out.println("Armstrong numbers between 100 to 999 are");
+		while (i < 999) {
+			num = i;
+			arm = 0;
+			while (num > 0) {
+				remain = num % 10;
+				arm = arm + (remain * remain * remain);
+				num = num / 10;
+			}
+			if (arm == i)
+				System.out.println(i + " is Armstrong number");
+			i++;
+		}
 	}
 }
