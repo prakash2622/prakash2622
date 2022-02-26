7f216a2115279eddbe3eb2ffb8b4de5aba1d7aeb
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-26 14:13:34 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -7,7 +7,7 @@ public class ArmstrongNumber{
 
 		while (temp != 0) {
 			remainder = temp % 10;
-			result += Math.pow(remainder, 3);
+			result += (remainder * remainder * remainder);
 			temp = temp / 10;
 		}
 		if (result == number)
