2cdc0ea3963a2e9b7df8c6de8fe9586cd8770743
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-26 13:15:54 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -19,3 +19,24 @@ public class PrimeNumbers {
 
 	}
 }
+
+// PrimeNumbers between 1 to 100
+
+public class Hiii {
+	public static void main(String[] args) {
+		System.out.println("Prime Number between 1 to 100");
+		int i, j, count = 0;
+		for (i = 2; i <= 100; i++) {
+			count = 0;
+			for (j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					count++;
+
+				}
+			}
+			if (count == 2)
+				System.out.println(i + " is a Prime Number");
+		}
+
+	}
+}
