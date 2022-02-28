7968ba72e3d710422ac36e67a6baf2688ad856c5
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-02-28 12:47:09 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,26 @@
+class Student{
+	private int rollno;
+	private String name;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+public class Encapsulation {
+	public static void main(String args[]) {
+		Student s1 = new Student();
+		s1.setRollno(5);
+		s1.setName("Karthik");
+		System.out.println("Rollno : "+s1.getRollno());
+		System.out.println("Student Name : "+s1.getName());
+	}
+
+}
