5aa6c5a57f1fdd60616a69dbef0e176198592aee
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-03-01 16:09:27 +0530

    Add files via upload

---
+++ ThrowAndThrows.java
@@ -0,0 +1,16 @@
+
+public class ThrowAndThrows {
+	static void method() throws ArithmeticException {
+		System.out.println("Inside the method()");
+		throw new ArithmeticException("throwing ArithmeticException");
+	}
+
+	// main method
+	public static void main(String args[]) {
+		try {
+			method();
+		} catch (ArithmeticException e) {
+			System.out.println("caught in main() method");
+		}
+	}
+}
