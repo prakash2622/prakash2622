1f710c5e2f9c13f8db6536cf2c96e14d695f6519
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-03-01 16:12:03 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,13 @@
+
+public class DefaultConstructor {
+	int num;
+	String  cars;
+
+	public static void main(String args[]) {
+		DefaultConstructor obj = new DefaultConstructor();
+		System.out.println("Default value : ");
+		System.out.println("Numer of Bikes = " + obj.num);
+		System.out.println("Cars= " + obj.cars);
+	}
+
+}
