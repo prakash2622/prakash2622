d6b29aaa8801bb1b3303d1ab62383812f98bb1bd
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-03-25 20:43:26 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,147 +1,88 @@
 import java.util.*;
 
 public class Student {
+	int rollno, age;
+	String name, gender;
 
-	static class Students {
-		private int roll_no;
-		private String name;
-
-		public int getRollno() {
-			return roll_no;
-		}
-
-		public void setRollno(int rollno) {
-			this.roll_no = rollno;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name_) {
-			this.name = name_;
-		}
-
-		public int getAge() {
-			return age;
-		}
-
-		public void setAge(int age_) {
-			this.age = age_;
-		}
-
-		public String getGender() {
-			return gender;
-		}
-
-		public void setGender(String gender_) {
-			this.gender = gender_;
-		}
-
-		private int age;
-		private String gender;
-	}
-
-	public static void main(String[] args) {
-
+	public static void main(String args[]) {
+		List<String> name = new ArrayList<String>();
+		List<Integer> roll_no = new ArrayList<Integer>();
+		List<String> gender = new ArrayList<String>();
+		List<Integer> age = new ArrayList<Integer>();
 		System.out.println("\t\t\t\t Student Details");
 		System.out.println();
 
-		Scanner input = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		ArrayList<Students> stud = new ArrayList<Students>();
-		try {
-			System.out.print("Number of Student : ");
-			int num = (Integer.parseInt(input.nextLine()));
-			for (int i = 0; i < num; i++) {
-				Students std = new Students();
-				System.out.print("Rollno : ");
-				std.setRollno(Integer.parseInt(input.nextLine()));
-				System.out.print("Name   : ");
-				std.setName(input.nextLine());
-				System.out.print("Age    : ");
-				std.setAge(Integer.parseInt(input.nextLine()));
-				System.out.print("gender : ");
-				std.setGender(input.nextLine());
-				stud.add(std);
-				System.out.println("******** Individual details entered successfully ********");
-				System.out.println();
-			}
-		} catch (NumberFormatException e) {
-			System.out.println("NumberFormatException occured");
-		}
-		System.out.println("+---------+-----------+-----+----------+");
-		System.out.println("| Roll.no | Name      | Age | Gender   |");
-		System.out.println("+---------+-----------+-----+----------+");
+		System.out.println("Number of Student");
+		int num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			System.out.println("Enter a Rollno:");
+			roll_no.add(sc.nextInt());
+			System.out.println("Enter a Student Name:");
+			sc.nextLine();
+			name.add(sc.nextLine());
+			System.out.println("Enter a age:");
+			age.add(sc.nextInt());
+			System.out.println("Enter a gender:");
+			sc.nextLine();
+			gender.add(sc.nextLine());
 
-		for (int i = 0; i < stud.size(); i++) {
-			System.out.println("| " + stud.get(i).getRollno() + "      | " + stud.get(i).getName() + "        | "
-					+ stud.get(i).getAge() + "  |  " + stud.get(i).getGender() + "       |");
-			System.out.println("+---------+-----------+-----+----------+");
+			System.out.println("********Individual details entered successfully********");
+			System.out.println();
 
 		}
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("NAME          | ROLL_NO           | AGE              | GENDER |");
+		System.out.println("--------------------------------------------------------------");
+
+		for (int i = 0; i < name.size(); i++) {
+			System.out.println(name.get(i) + "            |   " + roll_no.get(i) + "              |   " + age.get(i)
+					+ "             |   " + gender.get(i) + "   |");
+			System.out.println("--------------------------------------------------------------");
+
+		}
+
 		int menuChoice = 3;
-
 		do {
 			System.out.println("\t\t\tEnter a Student Details");
 			System.out.println("\t\t1. Add Student\t2. remove Students\t3. Exit");
 			System.out.println("Enter a choice: ");
-
-			menuChoice = input.nextInt();
+			Scanner input = new Scanner(System.in);
+			menuChoice = Integer.parseInt(input.nextLine());
 
 			if (menuChoice == 1) {
-				try {
-					Students std = new Students();
-					System.out.print("Rollno : ");
-					std.setRollno(input.nextInt());
-					System.out.print("Name   : ");
-					input.nextLine();
-					std.setName(input.nextLine());
-					System.out.print("Age    : ");
-					std.setAge(input.nextInt());
-					System.out.print("gender : ");
-					input.nextLine();
-					std.setGender(input.nextLine());
-					stud.add(std);
-					System.out.println("********Individual details entered successfully********");
-					System.out.println();
-				} catch (NumberFormatException e) {
-					System.out.println("NumberFormatException occured");
-				}
-				System.out.println("+---------+-----------+-----+----------+");
-				System.out.println("| Roll.no | Name      | Age | Gender   |");
-				System.out.println("+---------+-----------+-----+----------+");
-				System.out.println();
+				System.out.println("Enter a Rollno:");
+				roll_no.add(sc.nextInt());
+				System.out.println("Enter a Student Name:");
+				sc.nextLine();
+				name.add(sc.nextLine());
+				System.out.println("Enter a age:");
+				age.add(sc.nextInt());
+				System.out.println("Enter a gender:");
+				sc.nextLine();
+				gender.add(sc.nextLine());
+				System.out.println("--------------------------------------------------------------");
+				System.out.println("NAME          | ROLL_NO           | AGE              | GENDER |");
+				System.out.println("--------------------------------------------------------------");
 
-				for (int i = 0; i < stud.size(); i++) {
-					System.out.println("| " + stud.get(i).getRollno() + "      | " + stud.get(i).getName()
-							+ "        | " + stud.get(i).getAge() + "  |  " + stud.get(i).getGender() + "       |");
-					System.out.println("+---------+-----------+-----+----------+");
-				}
+				for (int i = 0; i < name.size(); i++) {
+					System.out.println(name.get(i) + "            |   " + roll_no.get(i) + "              |   "
+							+ age.get(i) + "             |   " + gender.get(i) + "   |");
+					System.out.println("--------------------------------------------------------------");
 
+				}
 			} else if (menuChoice == 2) {
-				System.out.print("Enter rollno number of the Student do you want edit : ");
-				int Roll = input.nextInt();
-				input.nextLine();
-				for (int i = 0; i < stud.size(); i++) {
-					if (stud.get(i).getRollno() == Roll) {
-						stud.remove(i);
-					}
-				}
-				System.out.println("+---------+-----------+-----+----------+");
-				System.out.println("| Roll.no | Name      | Age | Gender   |");
-				System.out.println("+---------+-----------+-----+----------+");
+				int menu = 4;
+				System.out.println("\t\t1. Remove rollno\t2. remove Name\t3. Remove age\t4. Remove gender");
+				Scanner s = new Scanner(System.in);
+				menu = Integer.parseInt(s.nextLine());
 
-				for (int i = 0; i < stud.size(); i++) {
-					System.out.println("| " + stud.get(i).getRollno() + "      | " + stud.get(i).getName()
-							+ "        | " + stud.get(i).getAge() + "  |  " + stud.get(i).getGender() + "       |");
-					System.out.println("+---------+-----------+-----+----------+");
-
-				}
 			}
 
 		} while (menuChoice < 3);
-		
-		System.out.println("Students details  are successfully uploaded....\nThank You..");
+		System.out.println("Student details successfully completed....");
+
 	}
+
 }
